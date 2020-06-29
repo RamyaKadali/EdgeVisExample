14b4025bccac0a9650dcd1a0c49af38c521eac66
RamyaKadali <ramya.kadali@digitalbarriers.com>
2020-06-29 16:28:50 +0100

    Commit

---
+++ EdgeVISClient_Test/Interface1.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Created by Ranorex
+ * User: RamyaKadali
+ * Date: 19/05/2020
+ * Time: 14:22
+ * 
+ * To change this template use Tools > Options > Coding > Edit standard headers.
+ */
+using System;
+
+namespace EdgeVISClient_Test
+{
+	/// <summary>
+	/// Description of Interface1.
+	/// </summary>
+	public interface Interface1
+	{
+		
+	}
+}
---
+++ EdgeVISClient_Test/UserCodeCollection1.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by Ranorex
+ * User: RamyaKadali
+ * Date: 19/05/2020
+ * Time: 14:21
+ * 
+ * To change this template use Tools > Options > Coding > Edit standard headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Drawing;
+using System.Threading;
+using WinForms = System.Windows.Forms;
+
+using Ranorex;
+using Ranorex.Core;
+using Ranorex.Core.Testing;
+
+namespace EdgeVISClient_Test
+{
+    /// <summary>
+    /// Creates a Ranorex user code collection. A collection is used to publish user code methods to the user code library.
+    /// </summary>
+    [UserCodeCollection]
+    public class UserCodeCollection1
+    {
+        // You can use the "Insert New User Code Method" functionality from the context menu,
+        // to add a new method with the attribute [UserCodeMethod].
+    }
+}

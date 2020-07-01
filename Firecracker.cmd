fa3a6dc475cb4af0c4b1b748193ac3fcda8b6cbb
RamyaKadali <67324805+RamyaKadali@users.noreply.github.com>
2020-07-01 15:57:59 +0100

    Update Firecracker.cmd

--- Firecracker.cmd
+++ Firecracker.cmd
@@ -1,1 +1,1 @@
-java -jar practitest-firecracker-standalone.jar --api-uri=https://eu1-prod-api.practitest.app/ --reports-path="Test1\bin\Debug" --testset-name="Test1" --author-id=26945 --config-path="%FIRECRACKER%" create-and-populate-testset
+java -jar practitest-firecracker-standalone.jar --api-uri=https://eu1-prod-api.practitest.app/ --reports-path="Test1\bin\Debug" --testset-name="Test1" --author-id=26945 --config-path="C:\Users\RamyaKadali\FC.config.json" create-and-populate-testset
